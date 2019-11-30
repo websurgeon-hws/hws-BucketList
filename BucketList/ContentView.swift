3d73be1657c7a5186ccf57391d70247bdc556cd9
Peter Barclay <peter@peter-barclay.com>
2019-11-30 14:45:04 +0000

    Writing data to the documents directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,27 +4,28 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister"),
-    ].sorted()
+    var body: some View {
+        Text("Hello World")
+        .onTapGesture {
+            let str = "Test Message"
+            let url = self.getDocumentsDirectory()      .appendingPathComponent("message.txt")
 
-   var body: some View {
-       List(users) { user in
-           Text("\(user.lastName), \(user.firstName)")
-       }
-   }
+            do {
+                try str.write(to: url, atomically: true, encoding: .utf8)
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        return paths[0]
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
