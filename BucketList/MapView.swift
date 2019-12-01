cba7bd99cf1e78e4ff868692059a0b80b5d2f0f0
Peter Barclay <peter@peter-barclay.com>
2019-12-01 14:56:34 +0000

    Integrating MapKit with SwiftUI

---
+++ BucketList/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  Copyright © 2019 Peter Barclay. All rights reserved.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
