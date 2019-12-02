a60dd209afbcb8deddbf37639197493444640892
Peter Barclay <peter@peter-barclay.com>
2019-12-02 19:58:09 +0000

    Advanced MKMapView with SwiftUI

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -2,42 +2,39 @@
 //  Copyright © 2019 Peter Barclay. All rights reserved.
 //
 
+import MapKit
 import SwiftUI
-import LocalAuthentication
 
 struct ContentView: View {
-    @State private var isUnlocked = false
-    
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+
     var body: some View {
-        VStack {
-            if self.isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
-        }.onAppear(perform: authenticate)
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "we need to unlock your data."
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
-                                   localizedReason: reason
-            ) { success, authenticationError in
-                DispatchQueue.main.async {
-                    if success {
-                        self.isUnlocked = true
-                    } else {
-                        // there was a problem
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
                     }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
                 }
             }
-        } else {
-            // no biometrics
         }
     }
 }
