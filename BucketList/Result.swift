52270db294a706ea0db79b9f9fef71f8c75274c0
Peter Barclay <peter@peter-barclay.com>
2019-12-03 08:28:12 +0000

    Downloading data from Wikipedia

---
+++ BucketList/Result.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2019 Peter Barclay. All rights reserved.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
