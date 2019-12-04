cd18687d57d8bfdc1b0035c2b86eed07dbc98723
Peter Barclay <peter@peter-barclay.com>
2019-12-04 08:46:23 +0000

    Locking our UI behind Face ID
    Use 'Simulator > Hardware > Face ID' to control face id

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -2,6 +2,7 @@
 //  Copyright © 2019 Peter Barclay. All rights reserved.
 //
 
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
@@ -11,38 +12,49 @@ struct ContentView: View {
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
+    @State private var isUnlocked = false
 
     var body: some View {
         ZStack {
-            MapView(centerCoordinate: $centerCoordinate,
-                    selectedPlace: $selectedPlace,
-                    showingPlaceDetails: $showingPlaceDetails,
-                    annotations: locations)
-            Circle()
-                .fill(Color.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            VStack {
-                Spacer()
-                HStack {
+            if self.isUnlocked {
+                MapView(centerCoordinate: $centerCoordinate,
+                        selectedPlace: $selectedPlace,
+                        showingPlaceDetails: $showingPlaceDetails,
+                        annotations: locations)
+                Circle()
+                    .fill(Color.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                VStack {
                     Spacer()
-                    Button(action: {
-                        let newLocation = CodableMKPointAnnotation()
-                        newLocation.title = "Example location"
-                        newLocation.coordinate = self.centerCoordinate
-                        self.locations.append(newLocation)
-                        self.selectedPlace = newLocation
-                        self.showingEditScreen = true
-                    }) {
-                        Image(systemName: "plus")
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            let newLocation = CodableMKPointAnnotation()
+                            newLocation.title = "Example location"
+                            newLocation.coordinate = self.centerCoordinate
+                            self.locations.append(newLocation)
+                            self.selectedPlace = newLocation
+                            self.showingEditScreen = true
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
                 }
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .clipShape(Capsule())
             }
         }
         .onAppear(perform: loadData)
@@ -56,7 +68,6 @@ struct ContentView: View {
                 EditView(placemark: self.selectedPlace!)
             }
         }
-        
     }
     
     func getDocumentsDirectory() -> URL {
@@ -86,6 +97,32 @@ struct ContentView: View {
             ])
         } catch {
             print("Unable to save data.")
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                                     error: &error) {
+            let reason = "Please authenticate yourself to unlock your places."
+
+            context.evaluatePolicy(
+                .deviceOwnerAuthenticationWithBiometrics,
+                localizedReason: reason
+            ) { success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            }
+        } else {
+            // no biometrics
         }
     }
 }
